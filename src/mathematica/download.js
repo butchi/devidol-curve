70fa2092d8b8d424d844ff19d0f5c7311c8b109b
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2019-04-10 21:45:27 +0900

    implement download script

--- src/mathematica/download.js
+++ src/mathematica/download.js
@@ -1,19 +1,23 @@
-// const fs = require('fs');
-// const ytdl = require('ytdl-core');
-// const BASE_PATH = `https://www.youtube.com/watch?v=`;
-
-const SAVE_PATH = 'src/mathematica/image/'
-
-const youtubeId = `gyDFoIbxB34`;
-// const url = BASE_PATH+youtubeId;
-
-// ytdl(url).pipe(fs.createWriteStream(`${SAVE_PATH}${youtubeId}.mp4`));
-
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
 
-const command = ffmpeg(`${SAVE_PATH}${youtubeId}.mp4`)
-  .videoCodec('png')
-  .fps(12)
-  .output('image-%i.png')
-  .run()
+const BASE_PATH = `https://www.youtube.com/watch?v=`;
+const SAVE_PATH = 'src/mathematica/image/'
+const youtubeId = `gyDFoIbxB34`;
+const url = BASE_PATH + youtubeId;
+
+ytdl(url)
+  .pipe(
+    fs.createWriteStream(`${SAVE_PATH}${youtubeId}.mp4`)
+      .on('close', _ => {
+        ffmpeg(`${SAVE_PATH}${youtubeId}.mp4`)
+          .screenshots({
+            count: 100,
+            folder: SAVE_PATH,
+            filename: 'image-%i.png',
+          })
+        ;
+      })
+  )
 ;
